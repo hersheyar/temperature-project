33cf9e175102e289aaabc9fe34ddf6d7eda32aba
hersheyar <hersheyar@outlook.com>
2024-11-03 10:27:39 -0600

    FSDI variables and Constants

---
+++ scripts/js/script.js
@@ -0,0 +1,70 @@
+console.log("Script");
+
+let myName = "Andrew";
+var myAge = 27;
+let isStudent = true;
+
+let myLastName = "Hershey";
+var myHeight = 6;
+
+
+console.log(myName);
+console.log(myAge,myHeight);
+
+console.log("My name is " + myName);
+
+console.log("My name is " + myName + " I am " + myAge + " years old. I am " + myHeight + " ft tall.");
+
+let num1 = 10;
+let num2 = 2;
+
+let addition = num1 + num2;
+let subtraction = num1 - num2;
+let multiplication = num1 * num2;
+let division = num1 / num2;
+let modulo = num1 % num2;
+
+console.log("Addition is, " + addition + " Subtraction is, "
+            + subtraction + " Multiplication is, " + multiplication +
+            " Division is, " + division + " Modulo is, " + modulo)
+
+let r = 3;
+const pi = Math.PI
+let a = pi * (r*r);
+
+console.log("This is the area " + a)
+
+
+// Declare Variables
+let char1 = "Leo";
+let char2 = "Stephanie";
+let char3 = "Jude";
+let action1 = "clutching";
+let action2 = "hesitated";
+let action3 = "gave a nod";
+let action4 = "gulped loudly";
+let object = "box ";
+let object1 = "very shiny brass lock";
+let object2 = "key";
+let object3 = "napkin";
+let quote = "'You've found me...'";
+let place = "Amusement park";
+let specificPlace = "streetlight,";
+
+console.log("-------------- STORY TIME -----------");
+
+console.log("In the " + place + " under the " + specificPlace + " " + char1 + ", " + char2 + ", and " + char3 +
+    " exchanged nervous looks. They'd found a " + object + "buried, with a " + object1 + " shining in the moonlight.");
+
+console.log("'Who wants to grab it?' " + char2 + " whispered, " + action1 + " the " + object2 + ".");
+
+console.log(char1 + " " + action2 + ", then " + action3 + " and said, 'I'll do it... but if I disappear, you’re paying for my funeral.'");
+
+let object1Again = "lock";
+
+console.log("The " + object1Again + " clicked, and the " + object + " opened, revealing a " + object3 +
+    " with a single line scribbled inside: " + quote);
+
+console.log("A chill ran through them as the wind stirred. " + char3 + " " + action4 + " and muttered, 'Maybe we should just put it back?'");
+
+console.log("They nervously walked back, never noticing the janitor who’d been watching in the background, thinking 'how did they get the key to my luch box.'");
